d074a224df6f3f84b1e83d7cecdacbe453eb8b55
Matias Veller <matias.veller@gmail.com>
2025-03-20 14:36:26 -0300

    Validacion de respuestas agregada

--- Ejercicio_entrega1.py
+++ Ejercicio_entrega1.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 # Preguntas para el juego
 questions = [
@@ -25,16 +26,21 @@ correct_answers_index = [1, 2, 0, 3, 1]
 for _ in range(3):
     # Se selecciona una pregunta aleatoria
     question_index = random.randint(0, len(questions) - 1)
-    
     # Se muestra la pregunta y las respuestas posibles
     print(questions[question_index])
     for i, answer in enumerate(answers[question_index]):
         print(f"{i + 1}. {answer}")
-    
     # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
-        user_answer = int(input("Respuesta: ")) - 1
-        
+        user_answer = input("Respuesta: ")
+        if not user_answer.isdigit():
+            print("Respuesta no válida")
+            sys.exit(1)
+        user_answer = int(user_answer) - 1
+        # Verifica si la respuesta está dentro del rango válido
+        if user_answer < 0 or user_answer >= 4:
+            print("Respuesta no válida")
+            sys.exit(1)        
         # Se verifica si la respuesta es correcta
         if user_answer == correct_answers_index[question_index]:
             print("¡Correcto!")
@@ -42,6 +48,5 @@ for _ in range(3):
         else:
             print("Incorrecto. La respuesta correcta es:")
             print(answers[question_index][correct_answers_index[question_index]])
-    
     # Se imprime un blanco al final de la pregunta
     print()
