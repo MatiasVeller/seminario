3f60a9d88d2d592d6cac5218b6897e431aca7862
Matias Veller <matias.veller@gmail.com>
2025-03-20 15:12:38 -0300

    Ya no se repitan las preguntas

--- Ejercicio_entrega1.py
+++ Ejercicio_entrega1.py
@@ -24,7 +24,7 @@ correct_answers_index = [1, 2, 0, 3, 1]
 
 puntaje = 0
 
-questions_to_ask = random.choices(list(zip(questions,answers, correct_answers_index)), k=3)
+questions_to_ask = random.sample(list(zip(questions,answers, correct_answers_index)), k=3)
 
 # El usuario deberá contestar 3 preguntas
 for question, possible_answers, correct_answer_index in questions_to_ask:
