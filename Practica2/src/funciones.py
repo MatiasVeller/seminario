5a0799400706c5fe67c7f361ba20e1760af41d6e
Matias Veller <matias.veller@gmail.com>
2025-04-04 19:11:18 -0300

    solucion de un error

--- Practica2/src/funciones.py
+++ Practica2/src/funciones.py
@@ -1,9 +1,5 @@
 def sort_key(player_stats):
-    puntos = player_stats[1] * 3 + player_stats[2] - player_stats[3]
-    return (puntos, player_stats[1], player_stats[2], - player_stats[3])  
+    return (player_stats[1] * 3 + player_stats[2] - player_stats[3]) 
 
 def final_sort_key(player_stats):
     return player_stats[1]['Puntos']
-
-
-
